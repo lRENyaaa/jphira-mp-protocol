57768abbc6b866ae8cbbbd8595a4564f83468d4d
RENaa_FD <92320175+lRENyaaa@users.noreply.github.com>
2026-02-02 21:37:22 +0800

    Add default packet behavior in simple packet handler

--- src/main/java/top/rymc/phira/protocol/handler/SimplePacketHandler.java
+++ src/main/java/top/rymc/phira/protocol/handler/SimplePacketHandler.java
@@ -3,6 +3,7 @@ package top.rymc.phira.protocol.handler;
 import io.netty.channel.Channel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import top.rymc.phira.protocol.packet.ServerBoundPacket;
 import top.rymc.phira.protocol.packet.clientbound.ClientBoundPongPacket;
 import top.rymc.phira.protocol.packet.serverbound.*;
 
@@ -19,76 +20,80 @@ public class SimplePacketHandler extends PacketHandler {
 
     @Override
     public void handle(ServerBoundAuthenticatePacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundChatPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundTouchesPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundJudgesPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundCreateRoomPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundJoinRoomPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundLeaveRoomPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundLockRoomPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundCycleRoomPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundSelectChartPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundRequestStartPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundReadyPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundCancelReadyPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundPlayedPacket packet) {
-
+        onUnhandledPacket(packet);
     }
 
     @Override
     public void handle(ServerBoundAbortPacket packet) {
+        onUnhandledPacket(packet);
+    }
 
+    protected void onUnhandledPacket(ServerBoundPacket packet) {
+        // Default behavior: ignore
     }
 }
